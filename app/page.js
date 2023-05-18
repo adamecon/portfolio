4f83de9422ea7ee26c8332ea800d138068fb6e62
adamecon <67404908+adamecon@users.noreply.github.com>
2023-05-18 20:47:37 +0300

    home page

--- app/page.js
+++ app/page.js
@@ -7,6 +7,10 @@ import {
   AiFillYoutube,
   AiFillGithub,
 } from "react-icons/ai";
+import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 export default function Home() {
   return (
@@ -39,7 +43,7 @@ export default function Home() {
               Adams Okeyo
             </h2>
             <h3 className="text-2xl py-2">Web And Mobile Developer</h3>
-            <p>
+            <p className="text-md py-5 leading-8 text-gray-800">
               I'm Adams Okeyo and I'm a Web and Mobile Developer. Motivated and
               passionate professional with a strong drive for success. Skilled
               in various areas of web and mobile development, I bring expertise
@@ -60,9 +64,41 @@ export default function Home() {
               in every project I undertake.
             </p>
           </div>
-          <div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
             <AiFillLinkedin />
             <AiFillGithub />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">What I Bring</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              With a passion for creating dynamic and user-friendly
+              applications, I excel{" "}
+              <span className="text-teal-500">
+                in front-end development, utilizing HTML, CSS, and JavaScript
+                frameworks
+              </span>
+              . I possess expertise in responsive design and optimizing
+              performance across various platforms. My strong problem-solving
+              skills enable me to troubleshoot and debug code efficiently.
+              Additionally, I have experience working with{" "}
+              <span className="text-teal-500">
+                back-end technologies such as Node.js and databases like MySQL
+                and MongoDB
+              </span>
+              . I am a collaborative team player, dedicated to delivering
+              high-quality results within project deadlines. Together, let us
+              build innovative and impactful digital experiences.
+            </p>
+          </div>
+          <div>
+            <div>
+              <Image src={code} />
+            </div>
           </div>
         </section>
       </main>
